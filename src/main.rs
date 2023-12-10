9169ae2a31fd50ee33ef41f76763920718aa8375
Yousef Moazzam <mrbenzedrine@live.co.uk>
2023-12-10 18:49:45 +0000

    Update CLI args length checker to catch too many args

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ fn main(){}
 
 mod helpers {
     pub fn check_number_of_args(args: &impl ExactSizeIterator) -> bool {
-        args.len() >= 2
+        args.len() == 2
     }
 }
 
@@ -15,6 +15,14 @@ mod tests {
         let dummy_args = ["/path/to/some/dir".to_string()];
         let iter = dummy_args.iter();
         let is_correct_no_of_args = check_number_of_args(&iter);
+        assert_eq!(is_correct_no_of_args, false);
+    }
+
+    #[test]
+    fn too_many_cli_args() {
+        let dummy_args = ["/input/dir/path", "/ouput/dir/path", "extra arg"];
+        let dummy_args = dummy_args.map(|s| s.to_string());
+        let is_correct_no_of_args = check_number_of_args(&dummy_args.iter());
         assert_eq!(is_correct_no_of_args, false);
     }
 }
