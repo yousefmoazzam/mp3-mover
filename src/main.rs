a0559df869681e0df6583f24cc38933d7859447e
Yousef Moazzam <mrbenzedrine@live.co.uk>
2023-12-29 20:26:32 +0000

    Add logging implementation

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use mp3_mover::config::Config;
 use mp3_mover::run;
 
 fn main(){
+    env_logger::init();
     let args: Vec<String> = env::args().collect();
     let config = Config::new(&args).unwrap_or_else(|err| {
         error!("Problem parsing args: {}", err);
