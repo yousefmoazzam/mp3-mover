f06c49063bd29f43340fa84b05c75ff2c3e237f3
Yousef Moazzam <mrbenzedrine@live.co.uk>
2023-12-29 20:01:58 +0000

    Add basic logic to run program in binary crate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-mod config;
+use std::env;
+use std::process;
 
-fn main(){}
+use log::{error, info};
+
+use mp3_mover::config::Config;
+use mp3_mover::run;
+
+fn main(){
+    let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        error!("Problem parsing args: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = run(config) {
+        error!("Encountered error: {}", e);
+        process::exit(1);
+    }
+
+    info!("Program completed sucecssfully");
+}
